87e0ae8f65fb39e6c52f0f81d3aadedfe239dcc6
Jinpyo, Kim <jpspace@nate.com>
2018-04-18 22:29:11 +0900

    블로그 개선하기

--- blog/urls.py
+++ blog/urls.py
@@ -6,5 +6,6 @@ urlpatterns = [
     url(r'^$', views.post_list, name='post_list'),
     url(r'^post/(?P<pk>\d+)/edit/$', views.post_edit, name='post_edit'),
     url(r'^post/new/$', views.post_new, name='post_new'),
+    url(r'^draft/$', views.post_draft_list, name='post_draft_list'),
     url(r'^post/(?P<pk>\d+)/$', views.post_detail, name='post_detail'),
 ]
